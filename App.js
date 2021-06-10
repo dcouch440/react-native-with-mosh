b46c14013d5ca7c69abb1a5488cb4d54f0bd48e1
dcouch440 <dcouch440@gmail.com>
2021-06-10 10:36:51 -0700

    add appText and example styles

--- App.js
+++ App.js
@@ -1,28 +1,59 @@
 import React from 'react';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
+import AppText from './app/components/AppText';
 
 import {
   SafeAreaView,
   StatusBar,
   StyleSheet,
   View,
-  Platform
+  Platform,
+  Text
 } from 'react-native';
 
 export default function App () {
   return (
     <SafeAreaView style={styles.test}>
-      <ViewImageScreen />
+      {/* <View style={[styles.blueCircle]} /> */}
+      <Text style={styles.textExample}>I love React Native</Text>
+      <AppText>YabaDatadsfg</AppText>
     </SafeAreaView>
     // <WelcomeScreen />
   );
 }
 
 const styles = StyleSheet.create({
+  textExample: {
+    fontSize: 30,
+    fontWeight: '600',
+    fontStyle: 'italic',
+    color: 'tomato',
+    textTransform: 'capitalize',
+    textDecorationLine: 'line-through',
+    textAlign: 'center',
+    lineHeight: 20
+  },
+  shadowExample: {
+    shadowColor: 'grey',
+    shadowOffset: { width: 0, height: 10 },
+    shadowOpacity: .5,
+    shadowRadius: 10,
+    // elevation is your only option for android
+    elevation: 4
+  },
   test: {
-    width: '100%',
-    height: '100%'
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  blueCircle: {
+    backgroundColor: 'dodgerblue',
+    height: 100,
+    width: 100,
+    borderWidth: 10,
+    borderColor: 10,
+    borderRadius: 50,
   },
   container: {
     flex: 1,
