2c31a080145788616ad8cccb037c4cfd49b8c909
dcouch440 <dcouch440@gmail.com>
2021-06-09 17:54:07 -0700

    add safe view

--- App.js
+++ App.js
@@ -1,27 +1,35 @@
 import React from 'react';
-import { useDimensions } from '@react-native-community/hooks';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 
 import {
   SafeAreaView,
+  StatusBar,
   StyleSheet,
-  View
+  View,
+  Platform
 } from 'react-native';
-import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App () {
   return (
-    <ViewImageScreen />
+    <SafeAreaView style={styles.test}>
+      <ViewImageScreen />
+    </SafeAreaView>
     // <WelcomeScreen />
   );
 }
 
 const styles = StyleSheet.create({
+  test: {
+    width: '100%',
+    height: '100%'
+  },
   container: {
     flex: 1,
+    width: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    // paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0
   },
 });
