5fc63e8b1514fe7377b2a2fd161601344d3b508b
dcouch440 <dcouch440@gmail.com>
2021-06-11 13:33:43 -0700

    add refresh tech

--- app/screens/MessagesScreen/index.js
+++ app/screens/MessagesScreen/index.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FlatList, View } from 'react-native'
 import ListItem from '../../components/ListItem';
 import styles from './styles';
-import messages from './messages';
+import initialMessages from './messages';
 import Screen from '../../components/Screen';
 import ListItemSeparator from '../../components/ListItemSeparator';
+import ListItemDeleteAction from '../../components/ListItemDeleteAction';
 
 export default function MessagesScreen () {
+  const [messages, setMessage] = useState([...initialMessages]);
+  const [refreshing, setRefreshing] = useState(false);
+
+  const handleDelete = message => {
+    // Delete the message from messages Array
+    setMessage(messages.filter(m => m.id !== message.id))
+  }
+
   return (
     <Screen>
       <FlatList
@@ -22,17 +31,15 @@ export default function MessagesScreen () {
               subTitle={item.description}
               image={item.image}
               onPress={() => console.log('Message selected', item)}
-              renderRightActions={() => (
-                <View style={{
-                  backgroundColor: 'red',
-                  width: 70
-                }}>
-                </View>
-              )}
+              renderRightActions={() => <ListItemDeleteAction onPress={() => handleDelete(item)} />}
             />
           )
         }
         ItemSeparatorComponent={ListItemSeparator}
+        refreshing={refreshing}
+        onRefresh={
+          () => setMessage([...initialMessages])
+        }
       />
     </Screen>
   );
