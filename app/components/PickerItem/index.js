6993652ae847fdd55ec94924d19e1818822f0953
dcouch440 <dcouch440@gmail.com>
2021-06-13 16:29:14 -0700

    add styles

--- app/components/PickerItem/index.js
+++ app/components/PickerItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import AppText from '../AppText/index'
+import styles from './styles';
 
 import {
   TouchableOpacity
@@ -8,7 +9,7 @@ import {
 export default function PickerItem ({ label, onPress }) {
   return (
     <TouchableOpacity onPress={onPress}>
-      <AppText>{ label }</AppText>
+      <AppText style={styles.text}>{ label }</AppText>
     </TouchableOpacity>
   );
 }
